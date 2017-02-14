2bad94b9933bbcde312dfdfca497f3a305c29aa2
SwatiSingh86 <er.swatisingh86@gmail.com>
2017-02-14 13:11:55 -0600

    Add files via upload

---
+++ assignments/shell/demo1.py
@@ -0,0 +1,41 @@
+import threading
+import os
+import sys
+import shutil
+
+
+def cp(cmd):
+     #Built-in function to copy souce to destination. Imported "shutil" library to use its built-in functions. 
+	shutil.copyfile(cmd[1],cmd[2])
+	#print('Copy Successful.')
+	return
+def pwd(cmd):
+	print(os.getcwd())
+	return
+	
+def cat(file):
+		f = open(file[1],'r')
+		print(f.read())
+		f.close()
+	
+	  
+if __name__ == '__main__':
+	cmd = input('% ')
+	# cmd=stdin()
+	cmd=cmd.split(' ')
+	if(cmd[0]=="cp"):
+		t = threading.Thread(target=cp,args=(cmd,))
+		t.start()
+		t.join()
+	elif (cmd[0]=="pwd"):
+		t = threading.Thread(target=pwd,args=(cmd,))
+		t.start()
+		t.join()
+	elif (cmd[0]=="cat"):
+		t = threading.Thread(target=cat,args=(cmd,))
+		t.start()
+		t.join()
+		
+	
+	else :
+		print("invalid command")
